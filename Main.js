4b938411f81f697705ccafbb3959a12ca4639ad5
Wagner Loch <wagner_loch@hotmail.com>
2026-03-17 09:39:46 -0300

    Adiciona mensagens extras

--- Main.js
+++ Main.js
@@ -1,1 +1,9 @@
-console.log("Hello, Git!");
+console.log("Olá, Git!");
+
+console.log("Bem-vindo ao GitHub!");
+
+console.log("Este é o meu primeiro repositório no GitHub!");
+
+console.log("Estou aprendendo a usar o Git e o GitHub!");
+
+console.log("Espero compartilhar muitos projetos interessantes aqui!");

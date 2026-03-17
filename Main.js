d529ca9b1a9b15c46230ed44587922865833477e
Wagner Loch <wagner_loch@hotmail.com>
2026-03-17 09:44:16 -0300

    Adiciona comentário de fluxo

--- Main.js
+++ Main.js
@@ -7,3 +7,10 @@ console.log("Este é o meu primeiro repositório no GitHub!");
 console.log("Estou aprendendo a usar o Git e o GitHub!");
 
 console.log("Espero compartilhar muitos projetos interessantes aqui!");
+
+// Fluxo de Commit
+
+// Modifica os arquivos do projeto e fazemos as alterações necessárias
+// Git Add: Adiciona os arquivos modificados para a área de staging
+// Git Commit: Salva as mudanças no repositório local
+

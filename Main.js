59b1fc7b64d40ac79370cd2dd993beac7d0f819f
Wagner Loch <wagner_loch@hotmail.com>
2026-03-17 11:30:07 -0300

    Adicionada novas funcionalidades

--- Main.js
+++ Main.js
@@ -26,3 +26,22 @@ function soma(a, b) {
 
 const resultado = soma(5, 3);
 console.log("O resultado da soma é: " + resultado);
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+const resultadoMultiplicacao = multiplicacao(4, 6);
+console.log("O resultado da multiplicação é: " + resultadoMultiplicacao);
+
+function divisao(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        console.log("Não é possível dividir por zero!");
+        return null;
+    }
+}
+
+const resultadoDivisao = divisao(10, 2);
+console.log("O resultado da divisão é: " + resultadoDivisao);
